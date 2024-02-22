eae0723b8524aea12cab59e2dbdb77f82a068eed
KayterCantFixBug <118961250+KayterCantFixBug@users.noreply.github.com>
2024-02-22 09:09:31 +0700

    a

---
+++ src/javadesignpatterns/facade/DwarvenTunnelDigger.java
@@ -0,0 +1,17 @@
+package javadesignpatterns.facade;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DwarvenTunnelDigger extends DwarvenMineWorker {
+
+  @Override
+  public void work() {
+	  LOGGER.info("{} creates another promising tunnel.", name());
+  }
+
+  @Override
+  public String name() {
+    return "Dwarven tunnel digger";
+  }
+}
