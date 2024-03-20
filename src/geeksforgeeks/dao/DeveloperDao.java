8d03999f7b6a00c9ade56b2bd15a14dbc9fce0f5
KayterCantFixBug <kienductrong@gmail.com>
2024-03-20 20:11:13 +0700

    a

---
+++ src/geeksforgeeks/dao/DeveloperDao.java
@@ -0,0 +1,10 @@
+package geeksforgeeks.dao;
+
+import java.util.List;
+
+interface DeveloperDao {
+    public List<Developer> getAllDevelopers();
+    public Developer getDeveloper(int DeveloperId);
+    public void updateDeveloper(Developer Developer);
+    public void deleteDeveloper(Developer Developer);
+}
---
+++ src/javatpoint/dao/SD.java
@@ -0,0 +1,8 @@
+package javatpoint.dao;
+import java.util.List;  
+public interface SD {  
+   public List<S> getAllStudents();  
+   public S getStudent(int r);  
+   public void updateStudent(S s);  
+   public void deleteStudent(S s);  
+}  
